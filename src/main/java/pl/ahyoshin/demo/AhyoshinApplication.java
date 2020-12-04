456e9cd0fcd47b394203817b9a21c90c9137c601
BigMurloc <s21159@pjwstk.edu.pl>
2020-12-04 18:00:47 +0100

    initial commit

---
+++ src/main/java/pl/ahyoshin/demo/AhyoshinApplication.java
@@ -0,0 +1,13 @@
+package pl.ahyoshin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhyoshinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhyoshinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ahyoshin/demo/AhyoshinApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ahyoshin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhyoshinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
