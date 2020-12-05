26bd810d3bd77ecc31164c58a63067c689632657
BigMurloc <s21159@pjwstk.edu.pl>
2020-12-05 14:10:34 +0100

    Added docker-compose.yaml with settings Added application.properties with settings I've chosen postgresql database as default and added corresponding dependencies to build.gradle Added db.changelog.yaml Added 20201205_init.sql Added package entities Added UserEntity to entities package Added UserRepository

---
+++ app/src/main/java/pl/ahyoshin/demo/UserRepository.java
@@ -0,0 +1,28 @@
+package pl.ahyoshin.demo;
+
+import org.springframework.stereotype.Repository;
+import pl.ahyoshin.demo.entities.UserEntity;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class UserRepository {
+    private final EntityManager em;
+
+    public UserRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public void saveUser(){
+        var userEntity = new UserEntity();
+        em.persist(userEntity);
+    }
+
+    public UserEntity findUserById(Long id){
+        return em.createQuery("select ue from UserEntity ue where ue.id = :id", UserEntity.class)
+                .setParameter("id", id)
+                .getSingleResult();
+    }
+
+
+}
