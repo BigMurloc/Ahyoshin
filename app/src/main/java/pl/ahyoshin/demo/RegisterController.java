b6bec141ef829fa404d9e23700921b73242b7d98
BigMurloc <s21159@pjwstk.edu.pl>
2020-12-05 14:53:05 +0100

    Moved RegisterController to controllers package

--- app/src/main/java/pl/ahyoshin/demo/RegisterController.java
+++
@@ -1,4 +0,0 @@
-package pl.ahyoshin.demo;
-
-public class RegisterController {
-}
---
+++ app/src/main/java/pl/ahyoshin/demo/controllers/RegisterController.java
@@ -0,0 +1,8 @@
+package pl.ahyoshin.demo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterController {
+
+}
