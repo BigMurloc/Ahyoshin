132077ad58d6161bee8e1e5f156a6899c595dd65
BigMurloc <s21159@pjwstk.edu.pl>
2020-12-11 22:00:47 +0100

    Modified UserRepository.saveUser to accept RegisterRequest parameter  Modified 20201205_add_user.sql to create username and password columns  Added api/register endpoint with register method which saves user provided by RegisterRequest body.

--- app/src/main/java/pl/ahyoshin/demo/controllers/RegisterController.java
+++ app/src/main/java/pl/ahyoshin/demo/controllers/RegisterController.java
@@ -1,8 +1,23 @@
 package pl.ahyoshin.demo.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.ahyoshin.demo.UserRepository;
+import pl.ahyoshin.demo.entities.UserEntity;
+import pl.ahyoshin.demo.requests.RegisterRequest;
 
 @RestController
 public class RegisterController {
+    private final UserRepository userRepository;
+
+    public RegisterController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/register")
+    public void register(@RequestBody RegisterRequest registerRequest){
+        userRepository.saveUser(registerRequest);
+    }
 
 }
--- app/src/main/java/pl/ahyoshin/demo/requests/RegisterRequest.java
+++ app/src/main/java/pl/ahyoshin/demo/requests/RegisterRequest.java
@@ -1,4 +1,20 @@
 package pl.ahyoshin.demo.requests;
 
 public class RegisterRequest {
+    private String username;
+    private String password;
+
+    public RegisterRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
