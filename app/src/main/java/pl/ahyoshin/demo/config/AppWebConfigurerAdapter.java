53c1c3ea1f8f0ea045bb22fa8c55b372534b49c5
BigMurloc <s21159@pjwstk.edu.pl>
2020-12-11 23:13:30 +0100

    added gradle dependency spring.WebSecurity

---
+++ app/src/main/java/pl/ahyoshin/demo/config/AppWebConfigurerAdapter.java
@@ -0,0 +1,9 @@
+package pl.ahyoshin.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+public class AppWebConfigurerAdapter extends WebSecurityConfigurerAdapter {
+}
