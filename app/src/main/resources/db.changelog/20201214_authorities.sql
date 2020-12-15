8590b526cc35f3cda77f1c7779bbf40200be673c
s21247 <56393403+s21247@users.noreply.github.com>
2020-12-15 19:45:06 +0100

    Update app/src/main/resources/db.changelog/20201214_authorities.sql
    Co-authored-by: BigMurloc <57963469+BigMurloc@users.noreply.github.com>

--- app/src/main/resources/db.changelog/20201214_authorities.sql
+++ app/src/main/resources/db.changelog/20201214_authorities.sql
@@ -1,5 +1,4 @@
-CREATE TABLE authorities(
-authorities_id BIGSERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE authority(
 app_user_id BIGSERIAL NOT NULL,
 authority VARCHAR NOT NULL,
 CONSTRAINT app_user_fk foreign key (app_user_id) REFERENCES app_user(id)
